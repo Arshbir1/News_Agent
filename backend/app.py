9dc75bb70d62d20c0a73a742e8e2b8c6dc360b5b
Arshbir1 <55049279+Arshbir1@users.noreply.github.com>
2025-02-23 16:39:58 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from elasticsearch import Elasticsearch
-from search import search_articles
+from .search import search_articles
 import logging
 import os  # For environment variable handling
 
